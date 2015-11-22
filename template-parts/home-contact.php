19fe47a0cb52a036f6801c9b39b22b75f35aa213
Jimmy Pocock <jimmypocock@yahoo.com>
2015-11-22 14:26:28 -0600

    Every bit of style seems to work in all browser widths

--- template-parts/home-contact.php
+++ template-parts/home-contact.php
@@ -10,20 +10,20 @@
   <div class="container">
     <h2>Contact Us</h2>
     <div class="header-bottom-border"></div>
-    <h4>We would love to answer any questions you may have!</h4>
+    <h4 class="subheader">We would love to answer any questions you may have!</h4>
     <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <h3>Adriana Jimenez</h3>
+      <div class="col-xs-12 col-sm-6 first">
+        <h3><strong>Adriana Jimenez</strong></h3>
         <h4>Content Manager</h4>
         <h4><a href="mailto:adriana.j@groupbrm.com">adriana.j@groupbrm.com</a></h4>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <h3>Joe DiPietro</h3>
+        <h3><strong>Joe DiPietro</strong></h3>
         <h4>Group Publisher</h4>
         <h4><a href="mailto:publisher@groupbrm.com">publisher@groupbrm.com</a></h4>
       </div>
     </div>
-    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/telephone-icon.png" alt="Telephone Icon">
+    <img class="telephone-icon" src="<?php bloginfo('stylesheet_directory'); ?>/img/telephone-icon.png" alt="Telephone Icon">
     <h4><a href="tel:17732945995">1.773.294.5995</a></h4>
   </div>
 </div>
