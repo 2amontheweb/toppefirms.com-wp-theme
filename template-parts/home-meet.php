dc3df2b601379ae9a650bfe098fa4d1a28e512d7
Jimmy Pocock <jimmypocock@yahoo.com>
2015-11-22 14:10:15 -0600

    Meet section is solidly looking.

--- template-parts/home-meet.php
+++ template-parts/home-meet.php
@@ -10,28 +10,30 @@
   <div class="container">
     <h2>Meet the Top 50 Program Producer</h2>
     <div class="header-bottom-border"></div>
-    <h4>THe Top 50 Program is produced by Bauer Rock Media Group. BRMG is one of the oldest and largest buyout-centric publishing firms in the US.</h4>
+    <h4>The Top 50 Program is produced by Bauer Rock Media Group. BRMG is one of the oldest and largest buyout-centric publishing firms in the US.</h4>
     <div class="row">
-      <div class="col-xs-12 col-sm-6">
+      <div class="col-xs-12 col-sm-6 left">
         <div class="download-container">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/img/download-our-media-kit.png" alt="Download Our Media Kit">
+          <img class="download-our-media-kit" src="<?php bloginfo('stylesheet_directory'); ?>/img/download-our-media-kit.png" alt="Download Our Media Kit">
           <a class="button-container" href="http://www.jimmypocock.com" target="_blank">
-            <div>Download our Media Kit to learn more about Bauer Rock Media Group.</div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/download-icon.png" alt="Download your PDF nomination form now.">
+            <h4>Download our Media Kit to learn more about Bauer Rock Media Group.</h4>
+            <img class="download-icon" src="<?php bloginfo('stylesheet_directory'); ?>/img/download-icon.png" alt="Download your PDF nomination form now.">
           </a>
         </div>
       </div>
-      <div class="col-xs-12 col-sm-6">
-        <div class="connecting-container">
-          <div class="text-container">
-            <h5>Connecting nearly 60,000 Middle Market Dealmakers Every Single Week Across the US</h5>
-            <div class="orgs">
-              <span class="first">M &amp; A Chicago</span>
-              <span>SBIC Weekly</span>
-            </div>
-            <div class="orgs">
-              <span class="first">Dealmaker South</span>
-              <span>The Top 50</span>
+      <div class="col-xs-12 col-sm-6 right">
+        <div class="connecting-container" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/img/top-50-producers.jpg);">
+          <div class="overlay">
+            <div class="text-container">
+              <h4><strong>Connecting nearly 60,000 Middle Market Dealmakers Every Single Week Across the US</strong></h4>
+              <h4 class="orgs">
+                <span class="first">M &amp; A Chicago</span>
+                <span>SBIC Weekly</span>
+              </h4>
+              <h4 class="orgs">
+                <span class="first">Dealmaker South</span>
+                <span>The Top 50</span>
+              </h4>
             </div>
           </div>
         </div>
