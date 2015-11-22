cdce2755382c0380cb0d996cb07619a98a501755
Jimmy Pocock <jimmypocock@yahoo.com>
2015-11-22 13:15:13 -0600

    Got the nominate section looking sharp. Mobilized pretty well.

--- template-parts/home-nominate.php
+++ template-parts/home-nominate.php
@@ -13,15 +13,15 @@
     <div class="header-bottom-border"></div>
     <h4>Tell us why your firm is a top 50 firm!</h4>
     <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/nominate-your-firm.jpg" alt="Nominate Your Firm">
+      <div class="col-xs-12 col-sm-6 left">
+        <img class="nominate" src="<?php bloginfo('stylesheet_directory'); ?>/img/nominate-your-firm.jpg" alt="Nominate Your Firm">
       </div>
-      <div class="col-xs-12 col-sm-6">
+      <div class="col-xs-12 col-sm-6 right">
         <div class="download-container">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/img/hurry-the-nomination-process-ends-soon.png" alt="Hurry! The nomination process ends soon!">
+          <img class="hurry" src="<?php bloginfo('stylesheet_directory'); ?>/img/hurry-the-nomination-process-ends-soon.png" alt="Hurry! The nomination process ends soon!">
           <a class="button-container" href="http://www.jimmypocock.com" target="_blank">
-            <div>Download your PDF nomination form now.</div>
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/download-icon.png" alt="Download your PDF nomination form now.">
+            <h4>Download your PDF nomination form now.</h4>
+            <img class="download-icon" src="<?php bloginfo('stylesheet_directory'); ?>/img/download-icon.png" alt="Download your PDF nomination form now.">
           </a>
         </div>
       </div>
